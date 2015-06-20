64ba263473c326e7364f27f5588e758242133ca8
Matt Parsons <parsonsmatt@gmail.com>
2015-06-20 00:54:01 -0400

    hmm

--- lib/squirrell.rb
+++ lib/squirrell.rb
@@ -1,5 +1,25 @@
 require "squirrell/version"
 
 module Squirrell
-  # Your code goes here...
+  attr_accessor :executor
+
+  def self.configure
+    yield self
+  end
+
+  def exec
+    finder || executor.execute(raw_sql)
+  end
+
+  def raw_sql
+    arel.to_sql
+  end
+
+  def result
+    process exec
+  end
+
+  def process(results)
+    results
+  end
 end
