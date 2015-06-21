6a0a09e0b0934c2623b78aaec77ab9ceaf901c64
Matt Parsons <parsonsmatt@gmail.com>
2015-06-20 21:24:19 -0400

    remove prints

--- lib/generators/squirrell/install/install_generator.rb
+++ lib/generators/squirrell/install/install_generator.rb
@@ -8,13 +8,11 @@ module Squirrell
       desc "Creates Squirrell initializer"
       def copy_initializer
         template "squirrell_initializer.rb", "config/initializers/squirrell.rb"
-        puts "Squirrell initializer installed."
       end
 
       desc "Creates Example Query"
       def example_query
         template "example_query.rb", 'app/queries/example_query.rb'
-        puts "Example query installed."
       end
     end
   end
